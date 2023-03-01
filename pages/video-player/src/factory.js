07dfd89b49779d516ed46748f99af9f8fa9f6733
João Mangueira <jamjs7@gmail.com>
2023-03-01 15:38:14 -0300

    colocando o botão na tela para inicializar

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,14 +1,33 @@
+import Camera from "../../../lib/shared/camera.js";
+import { supportsWorkerType } from "../../../lib/shared/util.js";
 import Controller from "./controller.js";
 import Service from "./service.js";
 import View from "./view.js";
-import Camera from "../../../lib/shared/camera.js";
+
+async function getWorker() {
+    if(supportsWorkerType()) {
+        console.log('support')
+        return new Worker('./src/worker.js', { type: 'module' })
+    }
+
+    const workerMock = {
+        async postMessage() {},
+        onmessage(msg) {}
+    }
+    console.log('not support')
+    return workerMock
+
+}
+
+const worker = await getWorker()
+worker.postMessage('hey form factory')
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
     async initialize() {
         return Controller.initialize({
-            view: new View({}),
+            view: new View(),
             service: new Service({})
         })
     }
--- pages/video-player/src/view.js
+++ pages/video-player/src/view.js
@@ -1,4 +1,15 @@
 export default class View {
-    constructor({ })  {
+    #btnInit = document.querySelector("#init")
+    #statusElement = document.querySelector("#status")
+    enableButton() {
+        this.#btnInit.disabled = false
+    }
+
+    configureOnBtnClick(fn) {
+        this.#btnInit.addEventListener('click', fn)
+    }
+
+    log(text) {
+        this.#statusElement.innerHTML = text
     }
 }
