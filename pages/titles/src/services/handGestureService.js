0480c5b5ffa685a818c31fb0cdd073a71d63c3f1
João Mangueira <jamjs7@gmail.com>
2023-03-02 10:42:02 -0300

    pegando as movimentações das mãos usando o webcam e add os gestos

---
+++ pages/titles/src/services/handGestureService.js
@@ -0,0 +1,36 @@
+import { knownGestures } from "../util/gestures.js";
+
+export default class HandGestureService {
+    #fingerpose
+    #handPoseDetection
+    #handsVersion
+    #detector = null
+    constructor({ fingerpose, handPoseDetection, handsVersion }) {
+        this.#fingerpose = fingerpose
+        this.#handPoseDetection = handPoseDetection
+        this.#handsVersion = handsVersion
+    }
+
+    async estimateHands(video) {
+        return this.#detector.estimateHands(video, {
+            flipHorizontal: true
+        })
+    }
+    async initializeDetector() {
+        if (this.#detector) return this.#detector
+
+        const detectorConfig = {
+            runtime: 'mediapipe', // or 'tfjs',
+            solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+            // full é o mais pesado e o mais preciso
+            modelType: 'lite',
+            maxHands: 2,
+        }
+        this.#detector = await this.#handPoseDetection.createDetector(
+            this.#handPoseDetection.SupportedModels.MediaPipeHands,
+            detectorConfig
+        )
+
+        return this.#detector
+    }
+}
