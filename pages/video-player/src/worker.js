f4fa37de572ef5bd58ed09ae0788f9879141f9bd
João Mangueira <jamjs7@gmail.com>
2023-03-02 09:21:53 -0300

    garantindo que funciona em todos navegadores

--- pages/video-player/src/worker.js
+++ pages/video-player/src/worker.js
@@ -3,7 +3,6 @@ import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
 import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
 import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
 import Service from "./service.js";
-import {addCwdToWatchOptions} from "browser-sync/dist/cli/transforms/addCwdToWatchOptions";
 
 const { tf, faceLandmarksDetection } = self
 tf.setBackend('webgl')
@@ -11,6 +10,7 @@ tf.setBackend('webgl')
 const service = new Service({
     faceLandmarksDetection
 })
+
 
 console.log('loading tf model')
 await service.loadModel()
