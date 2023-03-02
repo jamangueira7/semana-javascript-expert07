23846f81f7e26a19a8bc074aa425bcf6c3dc04d3
João Mangueira <jamjs7@gmail.com>
2023-03-02 15:35:54 -0300

    mãos na tela mas ainda não esta funcionando corretamente

--- pages/titles/src/controllers/handGestureController.js
+++ pages/titles/src/controllers/handGestureController.js
@@ -2,6 +2,7 @@ import { prepareRunChecker } from "../../../../lib/shared/util.js";
 
 
 const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+const { shouldRun: clickShouldRun } = prepareRunChecker({ timerDelay: 300 })
 
 export default class HandGestureController {
     #view
@@ -39,9 +40,20 @@ export default class HandGestureController {
     async #estimateHands() {
         try {
             const hands = await this.#service.estimateHands(this.#camera.video)
+            this.#view.clearCanvas()
+            if(hands?.length) {
+                this.#view.drawResults(hands)
+            }
             for await (const { event, x, y } of this.#service.detectGestures(hands)) {
-                if(event.includes('scroll')) {
-                    if(!scrollShouldRun()) continue
+                if (event === 'click') {
+                    if (!clickShouldRun()) continue
+                    this.#view.clickOnElement(x, y)
+
+                    continue
+                }
+
+                if (event.includes('scroll')) {
+                    if (!scrollShouldRun()) continue
                     this.#scrollPage(event)
                 }
             }
--- pages/titles/src/services/handGestureService.js
+++ pages/titles/src/services/handGestureService.js
@@ -1,14 +1,20 @@
-import { knownGestures, gestureStrings } from "../util/gestures.js";
-
 export default class HandGestureService {
     #gestureEstimator
     #handPoseDetection
     #handsVersion
     #detector = null
-    constructor({ fingerpose, handPoseDetection, handsVersion }) {
+    #gestureStrings
+    constructor({
+                    fingerpose,
+                    handPoseDetection,
+                    handsVersion,
+                    knownGestures,
+                    gestureStrings,
+                }) {
         this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
         this.#handPoseDetection = handPoseDetection
         this.#handsVersion = handsVersion
+        this.#gestureStrings = gestureStrings
     }
 
     async estimate(keypoints3D) {
@@ -34,7 +40,7 @@ export default class HandGestureService {
             const { x, y } = hand.keypoints.find(keypoint => keypoint.name === 'index_finger_tip')
             yield { event: result.name, x, y }
 
-            console.log('detected', gestureStrings[result.name])
+            console.log('detected', this.#gestureStrings[result.name])
         }
     }
 
