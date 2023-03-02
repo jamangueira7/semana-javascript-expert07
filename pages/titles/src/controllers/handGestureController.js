7baf221197e56c9cf1fcff4fb5fca464b9a9e3ea
João Mangueira <jamjs7@gmail.com>
2023-03-02 11:22:05 -0300

    scroll up e down funcinando com os gestos

--- pages/titles/src/controllers/handGestureController.js
+++ pages/titles/src/controllers/handGestureController.js
@@ -1,7 +1,16 @@
+import { prepareRunChecker } from "../../../../lib/shared/util.js";
+
+
+const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+
 export default class HandGestureController {
     #view
     #service
     #camera
+    #lastDirection = {
+        direction: '',
+        y: 0
+    }
     constructor({ view, service, camera }) {
         this.#view = view
         this.#service = service
@@ -11,10 +20,31 @@ export default class HandGestureController {
         return this.#loop()
     }
 
+    #scrollPage(direction) {
+        const pixelsPerScroll = 100
+
+        if(this.#lastDirection.direction === direction) {
+            this.#lastDirection.y = (
+                direction === 'scroll-down' ?
+                    this.#lastDirection.y + pixelsPerScroll :
+                    this.#lastDirection.y - pixelsPerScroll
+            )
+        } else {
+            this.#lastDirection.direction = direction
+        }
+
+        this.#view.scrollPage(this.#lastDirection.y)
+    }
+
     async #estimateHands() {
         try {
             const hands = await this.#service.estimateHands(this.#camera.video)
-            for await (const gesture of this.#service.detectGestures(hands)) {}
+            for await (const { event, x, y } of this.#service.detectGestures(hands)) {
+                if(event.includes('scroll')) {
+                    if(!scrollShouldRun()) continue
+                    this.#scrollPage(event)
+                }
+            }
         } catch (error) {
             console.error('deu ruim**', error)
         }
