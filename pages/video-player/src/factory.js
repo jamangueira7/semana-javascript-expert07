05d781bc58bbcdb80d20cb8dac3613561dd735f8
João Mangueira <jamjs7@gmail.com>
2023-03-03 13:07:39 -0300

    reorganizando o video

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -38,17 +38,21 @@ async function getWorker() {
     return workerMock
 }
 
-const worker = await getWorker()
+
 
 const camera = await Camera.init()
+const view = new View()
 const [rootPath] = window.location.href.split('/pages/')
+view.setVideoSrc(`${rootPath}/assets/video.mp4`)
+
+const worker = await getWorker()
+
 const factory = {
     async initialize() {
         return Controller.initialize({
             view: new View(),
             worker,
-            camera,
-            videoUrl: `${rootPath}/assets/video.mp4`
+            camera
         })
     }
 }
